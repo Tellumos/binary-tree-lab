7104e958e3708d0369d111dab5313775c2e20e01
mvrz <msv930@gmail.com>
2025-12-30 17:22:59 -0600

    binary_tree_lab

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -6,10 +6,24 @@ class TreeNode:
         self.left: Optional['TreeNode'] = None
         self.right: Optional['TreeNode'] = None
 
-# TODO: Implement the max_depth function
-def max_depth(root: Optional[TreeNode]) -> int:
-    pass
 
-# TODO: Implement the lowest_common_ancestor function
+def max_depth(root: Optional[TreeNode]) -> int:
+    if root is None:
+        return 0
+    left = max_depth(root.left)
+    right = max_depth(root.right)
+    return max(left, right) + 1
+
+
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    if root is None:
+        return 0
+    left = lowest_common_ancestor(root.left, p, q)
+    right = lowest_common_ancestor(root.right, p, q)
+    if ((p.val < root.val) and (q.val < root.val)):
+        return left
+    elif((p.val > root.val) and (q.val > root.val)):
+        return right
+    else:
+        return root
+
